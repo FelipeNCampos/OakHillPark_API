bf0cf8ee7f79b3e862b740a91b863cecf13ab74d
Felipe Nunes <felipe.n.cmp@gmail.com>
2026-03-09 16:39:54 -0300

    Merge head alembic

---
+++ backend/app/alembic/versions/bc3f8a1d9e42_merge_fire_alarm_and_flat_label_heads.py
@@ -0,0 +1,20 @@
+"""merge fire alarm and flat label heads
+
+Revision ID: bc3f8a1d9e42
+Revises: a9d4e6f2b1c3, ab9c7d4e2f31
+Create Date: 2026-03-09 13:10:00.000000
+
+"""
+
+revision = "bc3f8a1d9e42"
+down_revision = ("a9d4e6f2b1c3", "ab9c7d4e2f31")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
